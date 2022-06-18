961f2b9cf5f03735a8b7d3293ed93109a4d804e9
Johndell S. Kitts <k.jaydeee@gmail.com>
2022-06-18 11:22:26 +0800

    Contact tracing viewer ui is now complete

--- Contact Tracing/Viewer.Designer.vb
+++ Contact Tracing/Viewer.Designer.vb
@@ -22,9 +22,41 @@ Partial Class Viewer
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.formListBox = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'formListBox
+        '
+        Me.formListBox.FormattingEnabled = True
+        Me.formListBox.Location = New System.Drawing.Point(12, 12)
+        Me.formListBox.Name = "formListBox"
+        Me.formListBox.Size = New System.Drawing.Size(244, 23)
+        Me.formListBox.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(13, 42)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(443, 460)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Viewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(468, 514)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.formListBox)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Viewer"
         Me.Text = "Viewer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents formListBox As ComboBox
+    Friend WithEvents TextBox1 As TextBox
 End Class
